32ea9dc5ee20a94a9a90c4be43a8461639921910
Ashu <ashu976sincere@gmail.com>
2025-06-16 10:28:45 +0530

    changes

--- DjangoWebProject3/DjangoWebProject3/settings.py
+++ DjangoWebProject3/DjangoWebProject3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9c028312-bf53-4faa-99a0-1ca1887c6e39'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application references
 # https://docs.djangoproject.com/en/2.1/ref/settings/#std:setting-INSTALLED_APPS
